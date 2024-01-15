8805308c045886a0b586a91af22ec9f8a1fb6e67
sthandiwe-prog <132610235+sthandiwe-prog@users.noreply.github.com>
2024-01-15 03:00:55 +0200

    Created a currency converter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [ready, setReady] = useState(false);
+  const [ratesData, setRatesData] = useState({});
+  const [input, setInput] = useState("1");
+  function handleResponse(response) {
+    console.log(response.data.rates);
+
+    setRatesData({
+      base: response.data.base,
+      rates: response.data.rates,
+      date: response.data.date,
+    });
+    setReady(true);
+  }
+
+  // function handleOnChange(event) {
+  //   console.log(event.target.value);
+  //   setInput(event.target.value);
+  // }
+  function handleOnSubmit(event) {
+    setInput(event.target.value);
+  }
+
+  if (ready) {
+    return (
+      <div className="App">
+        <h1>Currency Convertion App</h1>
+        <div className="content">
+          <div className="base">
+            <label htmlFor="amount-to-convert">Amount in :</label> <br />
+            <input
+              type="text"
+              id="amount-to-convert"
+              // onChange={handleOnChange}
+            />
+          </div>
+          <div className="convert-button">
+            <button className="btn" type="sumbit" onSubmit={handleOnSubmit}>
+              Convert
+            </button>
+          </div>
+          <div className="converted-amount">
+            <h5>
+              {input} is 1234 {ratesData.base}
+            </h5>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiUrl = `http://data.fixer.io/api/latest?access_key=a3a1b102dbb8df0fb280cebe7c1bf2d9`;
+    axios.get(apiUrl).then(handleResponse);
+    return <p>Loading...</p>;
+  }
 }
-
-export default App;
